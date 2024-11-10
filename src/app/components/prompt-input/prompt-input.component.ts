7aa081267ab1ee293d5e4ff97d87bf4690081bf4
kewablay <kojokewajunior123@gmail.com>
2024-11-10 21:18:53 +0000

    feat: add gemini integration with response generation

--- src/app/components/prompt-input/prompt-input.component.ts
+++ src/app/components/prompt-input/prompt-input.component.ts
@@ -6,6 +6,8 @@ import {
   Validators,
 } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { GeminiService } from '../../services/gemini-service/gemini.service';
+import { ChatService } from '../../services/chat-service/chat.service';
 @Component({
   selector: 'app-prompt-input',
   standalone: true,
@@ -16,7 +18,10 @@ import { ButtonModule } from 'primeng/button';
 export class PromptInputComponent {
   promptForm: FormGroup;
   isLoading: boolean = false;
-  constructor() {
+  constructor(
+    private geminiService: GeminiService,
+    private chatService: ChatService
+  ) {
     this.promptForm = new FormGroup({
       prompt: new FormControl('', [Validators.required]),
     });
@@ -25,9 +30,29 @@ export class PromptInputComponent {
   submitPrompt() {
     if (this.promptForm.valid) {
       this.isLoading = true;
+      this.chatService.updateChatLoading(true);
       console.log(this.promptForm.value);
+      const { prompt } = this.promptForm.value;
+      this.promptForm.reset();
+
+      // add users message to chats
+      this.chatService.addUserMessage(prompt);
+
+      this.geminiService
+        .generateText(prompt)
+        .then((text) => {
+          this.chatService.addAIResponse(text);
+          this.isLoading = false;
+          this.chatService.updateChatLoading(false);
+          this.promptForm.reset();
+        })
+        .catch((error) => {
+          this.isLoading = false;
+          this.chatService.updateChatLoading(false);
+          this.chatService.updateChatError(error);
+          console.error("error: ",error);
+        });
     }
-  
   }
 
   ngOnInit() {}
