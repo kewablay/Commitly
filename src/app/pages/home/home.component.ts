d2cd11cdaad0ed411d66a2c6484c25c98219614a
kewablay <kojokewajunior123@gmail.com>
2024-11-10 21:21:43 +0000

    feat: added chats to home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "../../components/navbar/navbar.component";
-import { HeroSectionComponent } from "../../components/hero-section/hero-section.component";
-import { PromptInputComponent } from "../../components/prompt-input/prompt-input.component";
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { HeroSectionComponent } from '../../components/hero-section/hero-section.component';
+import { PromptInputComponent } from '../../components/prompt-input/prompt-input.component';
+import { Observable } from 'rxjs';
+import { ChatMessage } from '../../models/app.models';
+import { AsyncPipe } from '@angular/common';
+import { ChatService } from '../../services/chat-service/chat.service';
+import { ChatComponent } from '../../components/chat/chat.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, HeroSectionComponent, PromptInputComponent],
+  imports: [
+    NavbarComponent,
+    HeroSectionComponent,
+    PromptInputComponent,
+    AsyncPipe,
+    ChatComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.sass'
+  styleUrl: './home.component.sass',
 })
 export class HomeComponent {
-  
+  chat$!: Observable<ChatMessage[]>;
+
+  constructor(private chatService: ChatService, ) {}
+
+  ngOnInit() {
+    this.chat$ = this.chatService.chats$;
+
+  }
 }
