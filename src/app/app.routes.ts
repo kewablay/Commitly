d3fadc7c0c2e1c542f6740236d03adb96ead55a6
kewablay <kojokewajunior123@gmail.com>
2024-11-10 15:01:59 +0000

    feat(routes): add routing for authentication and other views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'chat/:id',
+    loadComponent: () =>
+      import('./pages/chat/chat.component').then((m) => m.ChatComponent),
+  },
+];
