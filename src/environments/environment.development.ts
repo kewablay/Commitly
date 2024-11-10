c57e9f8d0c0efec6510a7d576b34a1bb7857727a
kewablay <kojokewajunior123@gmail.com>
2024-11-10 21:32:25 +0000

    chore: update .gitignore

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'AIzaSyB3VBSXvk4e64RX2foBjJIqHsOFYsDTSBY',
+  genAiKey: 'AIzaSyB3VBSXvk4e64RX2foBjJIqHsOFYsDTSBY',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'AIzaSyB3VBSXvk4e64RX2foBjJIqHsOFYsDTSBY',
+  genAiKey: 'AIzaSyB3VBSXvk4e64RX2foBjJIqHsOFYsDTSBY'
+};
