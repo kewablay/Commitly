ab79531d59d0c24448faffed5d371efcd13b386d
kewablay <kojokewajunior123@gmail.com>
2024-11-11 10:21:46 +0000

    chore(gemini): update geminiService with generation config and system instructions

--- src/app/services/gemini-service/gemini.service.ts
+++ src/app/services/gemini-service/gemini.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import { environment } from '../../../environments/environment.development';
-import { formatPrompt } from '../../utils/initial-propmt';
+import { getSystemInstructions } from '../../utils/system-instructions';
 
 @Injectable({
   providedIn: 'root',
@@ -9,22 +9,31 @@ import { formatPrompt } from '../../utils/initial-propmt';
 export class GeminiService {
   genAi: GoogleGenerativeAI;
   apiKey = environment.genAiKey;
+  generationConfig = {
+    temperature: 0.8,
+    topP: 0.95,
+    topK: 25,
+    maxOutputTokens: 2048,
+  };
+
   constructor() {
     this.genAi = new GoogleGenerativeAI(this.apiKey);
   }
 
-  
-
   async generateText(prompt: string) {
     console.log('recieved prompt', prompt);
-    const userPrompt = formatPrompt(prompt);
-    const model = this.genAi.getGenerativeModel({ model: 'gemini-1.0-pro' });
+    const model = this.genAi.getGenerativeModel({ model: 'gemini-1.5-pro' });
 
-    const results = await model.generateContent(userPrompt + prompt); 
+    const results = await model.generateContent({
+      contents: [{ role: 'user', parts: [{ text: prompt }] }],
+      generationConfig: this.generationConfig,
+      systemInstruction: getSystemInstructions(),
+    });
     const response = await results.response;
     const text = response.text();
 
     console.log('text from ai', text);
     return text;
   }
+
 }
